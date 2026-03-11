9462d27dc36fc94ae42a3d386c46f4a616372d99
George Kaye <georgejkaye@gmail.com>
2026-03-11 23:06:08 +0000

    build: add prod data gen

--- api/populate.sh
+++ api/populate.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 VENUES_JSON="./venues.json"
-DB_NAME=realaletrail
-DB_USER=realaletrail
-DB_HOST=db
-DB_PASSWORD_FILE="temp.secret"
 
-echo "realaletrail" > $DB_PASSWORD_FILE
+if [ -z "$DB_PASSWORD_FILE" ]; then
+    DB_PASSWORD_FILE="temp.secret"
+    echo "realaletrail" > $DB_PASSWORD_FILE
+fi
+
 DB_PASSWORD_FILE_PATH=`realpath $DB_PASSWORD_FILE`
 
 poetry run python src/api/populate.py $VENUES_JSON $DB_HOST $DB_NAME $DB_USER $DB_PASSWORD_FILE_PATH
-
-rm $DB_PASSWORD_FILE_PATH
