ea71e347ba3e26d7ec4403a9c7f26c7c39ee6903
George Kaye <georgejkaye@gmail.com>
2026-03-20 13:50:46 +0000

    feat: add new tables and stuff for crawls

---
+++ db/data/45-generalise.sql
@@ -0,0 +1,66 @@
+CREATE TABLE IF NOT EXISTS crawl (
+    crawl_id SERIAL PRIMARY KEY,
+    crawl_name TEXT NOT NULL UNIQUE,
+    crawl_dates DATERANGE NOT NULL,
+    is_public BOOLEAN NOT NULL,
+    crawl_bg TEXT,
+    crawl_fg TEXT
+);
+
+INSERT INTO crawl (
+    crawl_id,
+    crawl_name,
+    crawl_dates,
+    is_public,
+    crawl_bg,
+    crawl_fg
+)
+VALUES (
+    1,
+    'Real Ale Trail 2026',
+    '[2026-03-01,2026-05-31]',
+    't',
+    '#f9eed2',
+    '#282e54'
+);
+
+CREATE TABLE IF NOT EXISTS crawl_special_venue_type (
+    crawl_special_venue_type_id SERIAL PRIMARY KEY,
+    crawl_id INTEGER NOT NULL,
+    display_text TEXT NOT NULL,
+    FOREIGN KEY (crawl_id) REFERENCES crawl(crawl_id),
+    UNIQUE (crawl_id, display_text)
+);
+
+CREATE TABLE IF NOT EXISTS crawl_venue (
+    crawl_id INTEGER NOT NULL,
+    venue_id INTEGER NOT NULL,
+    crawl_special_venue_type_id INTEGER,
+    FOREIGN KEY (crawl_id) REFERENCES crawl(crawl_id),
+    FOREIGN KEY (venue_id) REFERENCES venue(venue_id),
+    FOREIGN KEY (crawl_special_venue_type_id)
+        REFERENCES crawl_special_venue_type(crawl_special_venue_type_id)
+);
+
+INSERT INTO crawl_venue (
+    crawl_id,
+    venue_id
+)
+SELECT
+    1,
+    venue_id
+FROM venue;
+
+ALTER TABLE visit
+ADD COLUMN IF NOT EXISTS crawl_id INTEGER;
+
+ALTER TABLE visit
+ADD CONSTRAINT fk_visit_crawl_id
+FOREIGN KEY (crawl_id)
+REFERENCES crawl(crawl_id);
+
+UPDATE visit
+SET crawl_id = 1;
+
+ALTER TABLE visit
+ALTER COLUMN crawl_id SET NOT NULL;
--- db/schema/all.sql
+++ db/schema/all.sql
@@ -9,6 +9,23 @@ CREATE TABLE app_user (
     last_verify_request TIMESTAMP WITH TIME ZONE
 );
 
+CREATE TABLE crawl (
+    crawl_id SERIAL PRIMARY KEY,
+    crawl_name TEXT NOT NULL UNIQUE,
+    crawl_dates DATERANGE NOT NULL,
+    is_public BOOLEAN NOT NULL,
+    crawl_bg TEXT,
+    crawl_fg TEXT
+);
+
+CREATE TABLE crawl_special_venue_type (
+    crawl_special_venue_type_id SERIAL PRIMARY KEY,
+    crawl_id INTEGER NOT NULL,
+    display_text TEXT NOT NULL,
+    FOREIGN KEY (crawl_id) REFERENCES crawl(crawl_id),
+    UNIQUE (crawl_id, display_text)
+);
+
 CREATE TABLE venue (
     venue_id SERIAL PRIMARY KEY,
     venue_name TEXT NOT NULL,
@@ -18,14 +35,26 @@ CREATE TABLE venue (
     UNIQUE (venue_name, venue_address)
 );
 
+CREATE TABLE crawl_venue (
+    crawl_id INTEGER NOT NULL,
+    venue_id INTEGER NOT NULL,
+    crawl_special_venue_type_id INTEGER,
+    FOREIGN KEY (crawl_id) REFERENCES crawl(crawl_id),
+    FOREIGN KEY (venue_id) REFERENCES venue(venue_id),
+    FOREIGN KEY (crawl_special_venue_type_id)
+        REFERENCES crawl_special_venue_type(crawl_special_venue_type_id)
+);
+
 CREATE TABLE visit (
     visit_id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
+    crawl_id INTEGER NOT NULL,
     venue_id INTEGER NOT NULL,
     visit_date TIMESTAMP WITH TIME ZONE,
     notes TEXT,
     rating INTEGER,
     drink TEXT,
     FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    FOREIGN KEY (crawl_id) REFERENCES crawl(crawl_id),
     FOREIGN KEY (venue_id) REFERENCES venue(venue_id)
 );
