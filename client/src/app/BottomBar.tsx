dafed6b2c28dbf607ac9fa94190b24fff2226269
George Kaye <georgejkaye@gmail.com>
2026-03-23 21:08:19 +0000

    feat: add border to bottom bar

--- client/src/app/BottomBar.tsx
+++ client/src/app/BottomBar.tsx
@@ -26,7 +26,7 @@ const BottomBar = () => {
   return (
     <div>
       <div className="h-[60px]" />
-      <div className="fixed bottom-0 w-full md:hidden h-[60px] bg-accent text-accentfg font-bold">
+      <div className="fixed bottom-0 w-full md:hidden h-[60px] bg-accent text-accentfg font-bold border-accentlighter border-t-2">
         {isLoadingUser ? (
           <Loader />
         ) : (
