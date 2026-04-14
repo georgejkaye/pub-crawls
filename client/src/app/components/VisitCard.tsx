f9c10c8110a18c95b595f64081a334f3e74f1970
George Kaye <georgejkaye@gmail.com>
2026-04-14 21:23:57 +0100

    feat: show year in visits

--- client/src/app/components/VisitCard.tsx
+++ client/src/app/components/VisitCard.tsx
@@ -79,6 +79,7 @@ export const VisitCardReview = ({ review }: VisitCardReviewProps) => {
             weekday: "long",
             day: "2-digit",
             month: "long",
+            year: "numeric",
           })}
           {", "}
           {visitDate.toLocaleTimeString("en-UK", {
