49778b02cab5ef2904d22ade0c583cda61d57f25
George Kaye <georgejkaye@gmail.com>
2026-04-15 23:36:19 +0100

    feat: remove unused top bar feature

--- client/src/app/TopBar.tsx
+++ client/src/app/TopBar.tsx
@@ -33,7 +33,7 @@ const TopBar = () => {
         {!isLoadingUser && (
           <div className="flex flex-row gap-4">
             <div className="hidden md:flex flex-row gap-4">
-              {currentCrawl !== undefined ? (
+              {currentCrawl && (
                 <div>
                   <Link
                     className={`${linkStyle} font-bold border-3 p-2 rounded-xl`}
@@ -42,8 +42,6 @@ const TopBar = () => {
                     {currentCrawl.crawl_name}
                   </Link>
                 </div>
-              ) : (
-                <div>Click to choose crawl</div>
               )}
               <Link className={linkStyle} href="/">
                 Map
